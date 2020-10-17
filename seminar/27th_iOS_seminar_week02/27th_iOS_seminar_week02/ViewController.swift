5a4b05d913dc52491fa518f6d905b98883f67bf8
inaeLee <inae9709@hanmail.net>
2020-10-17 23:45:52 +0900

    add: 2nd seminar (auto layout, stack view, scroll view)

---
+++ seminar/27th_iOS_seminar_week02/27th_iOS_seminar_week02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  27th_iOS_seminar_week02
+//
+//  Created by inae Lee on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
