0f47f36c415afbf89a2331837ff49c7d9caacae6
inaeLee <inae9709@hanmail.net>
2020-10-26 00:14:10 +0900

    add: 2주차 기본 과제

---
+++ assignment/27th_iOS_assignment_week02/27th_iOS_assignment_week02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  27th_iOS_assignment_week02
+//
+//  Created by inae Lee on 2020/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
