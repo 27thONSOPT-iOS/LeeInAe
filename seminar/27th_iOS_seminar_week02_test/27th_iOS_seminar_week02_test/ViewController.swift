9635c9d6f34be58be2ad2e490547cb6cc88f380a
inaeLee <inae9709@hanmail.net>
2020-10-26 00:13:43 +0900

    prac: scrollView 연습하기

---
+++ seminar/27th_iOS_seminar_week02_test/27th_iOS_seminar_week02_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  27th_iOS_seminar_week02_test
+//
+//  Created by inae Lee on 2020/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
