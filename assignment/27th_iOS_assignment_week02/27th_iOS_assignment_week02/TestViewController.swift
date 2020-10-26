cbb852211b8321a9b0f95721307d00bef3a6992d
inaeLee <inae9709@hanmail.net>
2020-10-27 02:06:31 +0900

    add: 2주차 도전 과제 (delegate, contentOffset)

---
+++ assignment/27th_iOS_assignment_week02/27th_iOS_assignment_week02/TestViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TestViewController.swift
+//  27th_iOS_assignment_week02
+//
+//  Created by inae Lee on 2020/10/26.
+//
+
+import Foundation
--- assignment/27th_iOS_assignment_week02/27th_iOS_assignment_week02/ViewController.swift
+++ assignment/27th_iOS_assignment_week02/27th_iOS_assignment_week02/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        self.initButton(topButton);
+        
+        print("banner frame: \(bannerImage.frame.height) \(bannerImage.frame.origin.y)")
+        
+        print("banner bound: \(bannerImage.bounds.origin.x) \(bannerImage.bounds.origin.y)")
+        
+        print("scroll bound: \(scrollView.bounds.origin.x), \(scrollView.bounds.origin.y)")
     }
-
-
+    
+    func initButton(_ button: UIButton) {
+        button.isHidden = true
+        button.layer.cornerRadius = 0.5 * (button.bounds.size.width)
+    }
+    
+    @IBAction func setScrollToTop(_ sender: UIButton) {
+        scrollView.setContentOffset(.zero, animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (scrollView.bounds.origin.y > bannerImage.frame.height) {
+            topButton.isHidden = false
+        } else {
+            topButton.isHidden = true
+        }
+    }
 }
 
